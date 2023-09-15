07b6b27472633d74b4d05012dff413713978d7a4
ohiagosouza <ohiagossouza@gmail.com>
2023-09-15 15:01:59 -0300

    Style change and Javascript update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-const buyButton = document.querySelector('#buyButton');
+const buyButton = document.querySelectorAll('.buyButton');
 const addedToCart = document.querySelector('.toast');
 
-buyButton.addEventListener('click', () => {
-  const toast = bootstrap.Toast.getOrCreateInstance(addedToCart);
-  toast.show();
+buyButton.forEach(buyButton => {
+  buyButton.addEventListener('click', () => {
+    const notification = document.getElementById('notification');
+
+    const quantity = document.querySelectorAll('').value;
+    console.log({ quantity });
+
+    notification + quantity;
+    const toast = bootstrap.Toast.getOrCreateInstance(addedToCart);
+    toast.show();
+
+    notification.innerText = quantity;
+  });
 });
 
 console.log({ buyButton });
 console.log({ addedToCart });
+console.log({ notification });
