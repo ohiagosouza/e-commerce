f61a8888b413171c961b359458f7162a5f8098d7
ohiagosouza <ohiagossouza@gmail.com>
2023-09-27 21:02:51 -0300

    Javascript update

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,73 @@
-const shoppingBag = JSON.parse(localStorage.getItem("cart-products"));
-const cartQuantity = JSON.parse(localStorage.getItem("cart-quantity"));
+const shoppingCart = JSON.parse(localStorage.getItem("shoppingCartData"));
+const cartQuantity = JSON.parse(localStorage.getItem("shoppingCartQuantity"));
 
-console.log(shoppingBag, { cartQuantity });
+console.log(shoppingCart);
+
+for (let item of shoppingCart) {
+  let totalValue = item.price * item.quantity;
+  console.log(totalValue);
+}
+
+function cartSubtotal() {
+  const subtotal = document.getElementById("subtotal");
+  subtotal.textContent = totalValue;
+}
+
+if (shoppingCart && cartQuantity) {
+  const cartContainer = document.querySelector(".products");
+
+  shoppingCart.forEach(product => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("cart-item");
+
+    const productImage = document.createElement("img");
+    productImage.classList.add("productImage");
+    productImage.alt = "Produto";
+    productImage.src = product.image;
+
+    const productTitle = document.createElement("p");
+    productTitle.classList.add("product-title");
+    productTitle.textContent = product.title;
+
+    const productQuantity = document.createElement("input");
+    productQuantity.classList.add("form-control", "product-quantity");
+    productQuantity.type = "number";
+    productQuantity.ariaLabel = "quantity";
+    productQuantity.value = "1";
+    productQuantity.min = "1";
+    productQuantity.max = "10";
+
+    const classPrice = document.createElement("p");
+    classPrice.classList.add("card-price");
+    classPrice.textContent = "R$ ";
+
+    const itemPrice = document.createElement("span");
+    itemPrice.textContent = product.price;
+    itemPrice.classList.add("item-price");
+
+    const removeProductBtn = document.createElement("button");
+    removeProductBtn.classList.add("removeButton", "btn", "btn-primary");
+    removeProductBtn.innerHTML = '<img src="./assets/trashIcon.png" alt="Excluir produto" width="20" />';
+
+    classPrice.appendChild(itemPrice);
+    productElement.append(productImage, productTitle, productQuantity, classPrice, removeProductBtn);
+
+    cartContainer.appendChild(productElement);
+  });
+} else {
+  const cartContainer = document.querySelector(".products");
+  const cartInfo = document.querySelector(".cart-info");
+  cartInfo.innerHTML = "";
+
+  const productElement = document.createElement("div");
+  productElement.classList.add("cart-item", "d-flex");
+
+  const emptyCart = document.createElement("p");
+  emptyCart.classList.add("h6", "text-center");
+  emptyCart.innerText = "Seu carrinho está vazio, que tal escolher algo? 😉";
+
+  productElement.appendChild(emptyCart);
+  cartContainer.appendChild(productElement);
+}
+
+cartSubtotal();
--- js/main.js
+++ js/main.js
@@ -2,20 +2,19 @@ const addedToCart = document.querySelector(".toast");
 const notification = document.getElementById("notification");
 const addToCartButton = document.querySelectorAll(".buyButton");
 
-const shoppingCart = [];
-let totalCartItens = localStorage.getItem("cart-quantity");
+export const shoppingCart = [];
+let totalCartItens = localStorage.getItem("shoppingCartQuantity");
 
 function addToCart(title, price, image) {
   shoppingCart.push({
     title: title,
     price: price,
     image: image,
+    quantity: 1,
   });
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  // event.preventDefault();
-  console.log({ shoppingCart });
+document.addEventListener("DOMContentLoaded", function () {
   notification.innerText = totalCartItens;
 });
 
@@ -34,17 +33,20 @@ addToCartButton.forEach(addToCartButton => {
 
     const image = card.querySelector(".card-image").getAttribute("src");
     const title = card.querySelector(".card-title").textContent;
-    const price = card.querySelector(".card-price").textContent;
+    const price = card.querySelector(".item-price").textContent;
 
     //Atualiza a quantidade de produtos no carrinho
     totalCartItens = shoppingCart.length + 1;
     notification.innerText = totalCartItens;
     addToCart(title, price, image);
 
+    localStorage.setItem("shoppingCartData", JSON.stringify(shoppingCart));
+    localStorage.setItem("shoppingCartQuantity", JSON.stringify(totalCartItens));
+
     // Console log para entender o que está acontecendo no código
     console.log({ title, price, image }, addToCartButton);
-
-    localStorage.setItem("cart-products", JSON.stringify(shoppingCart));
-    localStorage.setItem("cart-quantity", JSON.stringify(totalCartItens));
   });
 });
+
+console.log(shoppingCart);
+console.log(totalCartItens);
